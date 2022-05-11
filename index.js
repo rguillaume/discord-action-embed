419d11c5d67fb51a19bb036ea932aa57f7f5c66b
Romain Guillaume <romain@edapp.com>
2022-05-11 21:28:58 +1000

    update failed

--- index.js
+++ index.js
@@ -1,38 +1,36 @@
-
-const core = require('@actions/core');
-const github = require('@actions/github');
-const axios = require('axios');
-const template = require('lodash.template');
+const core = require("@actions/core");
+const github = require("@actions/github");
+const axios = require("axios");
+const template = require("lodash.template");
 
 try {
-    const webhook = core.getInput('webhook');
-    const message = core.getInput('message');
-    const embed = core.getInput('embed');
+  const webhook = core.getInput("webhook");
+  const message = core.getInput("message");
+  const embed = core.getInput("embed");
 
-    const githubPayload = github.context.payload;
+  const githubPayload = github.context.payload;
 
-    const content = template(message)(githubPayload);
-    const embedJson = JSON.parse(template(embed)(githubPayload));
+  const content = template(message)(githubPayload);
+  const embedJson = JSON.parse(template(embed)(githubPayload));
 
-    const data = {
-        content,
-        embeds: [embedJson]
-    }
+  const data = {
+    content,
+    embeds: [embedJson],
+  };
 
-    const options = {
-        method: 'POST',
-        url: webhook,
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        data
-    };
+  const options = {
+    method: "POST",
+    url: webhook,
+    headers: {
+      "Content-Type": "application/json",
+    },
+    data,
+  };
 
-    axios
-        .request(options)
-        .then(() => {
-            core.info('Message was sent successfully');
-        });
+  axios.request(options).then(() => {
+    core.info("Message was sent successfully");
+  });
 } catch (error) {
-    core.setFailed(error.message);
+  // core.setFailed(error.message);
+  core.info("Message failed");
 }
